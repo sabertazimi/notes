6c08aecfae12e80e97227fd9d23adcbb46fc334d
sabertazimi <sabertazimi@gmail.com>
2026-02-28 20:30:56 +0800

    chore(deps): upgrade to markdownlint-cli2

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -3,9 +3,7 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  'src/**/*.{ts,tsx}': 'eslint --fix',
   'src/**/*.css': 'stylelint --fix',
-  'content/web/**/*.md': 'eslint --fix',
   'content/web/css/**/*.md': 'stylelint --fix',
-  'content/**/*.md': 'markdownlint --fix',
+  'content/**/*.md': 'markdownlint-cli2 --fix',
 }
