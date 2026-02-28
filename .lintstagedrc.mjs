e1135c668e09c7ec8309d8528f863e05ba1a31e8
sabertazimi <sabertazimi@gmail.com>
2026-02-28 11:23:19 +0800

    feat(react-hook): optimistic and transition for async React

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -3,7 +3,9 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
+  'src/**/*.{ts,tsx}': 'eslint --fix',
   'src/**/*.css': 'stylelint --fix',
+  'content/web/**/*.md': 'eslint --fix',
   'content/web/css/**/*.md': 'stylelint --fix',
   'content/**/*.md': 'markdownlint --fix',
 }
